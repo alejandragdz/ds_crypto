ce12fd9f0d8b87370f6e2809dac92285c1f0d041
alejandragdz <yazmin.alejandragh@gmail.com>
2024-02-04 15:28:42 -0600

    Cálculo de balance

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,8 +6,9 @@ class HomeController < ApplicationController
       @investments = []
       @table = CSV.parse(File.read("app/assets/docs/origen.csv"), headers: true)
       @table.each do |row|
-        investment     = Investment.new(coin: row["Moneda"], interest: row["Interes_mensual"].to_f, balance_init: row["balance_ini"].to_f)
-        investment.pay = ((investment.interest / 100)*investment.balance_init)*12
+        investment         = Investment.new(coin: row["Moneda"], interest: row["Interes_mensual"].to_f, balance_init: row["balance_ini"].to_f)
+        investment.pay     = ((investment.interest / 100)*investment.balance_init)*12
+        investment.balance = investment.balance_init + investment.pay
         @investments.push(investment)
       end
     rescue => e
--- app/models/investment.rb
+++ app/models/investment.rb
@@ -5,7 +5,8 @@ class Investment
   field :interest, type: Float
   field :balance_init, type: Float
   field :pay, type: Float
+  field :balance, type: Float
 
   validates :coin, :interest, :balance_init, presence: true
-  validates :interest, :balance_init, :pay, numericality: { greater_than: 0 }
+  validates :interest, :balance_init, :pay, :balance, numericality: { greater_than: 0 }
 end
