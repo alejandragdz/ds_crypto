a983a1f235e93822f6bf806ac8dde6d3af11f704
alejandragdz <yazmin.alejandragh@gmail.com>
2024-02-04 14:52:31 -0600

    Se muestra la lectura de documento csv en una tabla

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,9 +3,11 @@ class HomeController < ApplicationController
 
   def index
     begin
+      @investments = []
       @table = CSV.parse(File.read("app/assets/docs/origen.csv"), headers: true)
       @table.each do |row|
-        investment = Investment.create!(coin: row["Moneda"], interest: row["Interes_mensual"].to_f, balance_init: row["balance_ini"].to_f)
+        investment = Investment.new(coin: row["Moneda"], interest: row["Interes_mensual"].to_f, balance_init: row["balance_ini"].to_f)
+        @investments.push(investment)
       end
     rescue => e
       redirect_to error_page_url, notice: e
