c1557d03e2ba9e4bb36fb9565c4a2c857c0b8aff
alejandragdz <yazmin.alejandragh@gmail.com>
2024-02-04 15:22:20 -0600

    Cálculo de ganancia

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,7 +6,8 @@ class HomeController < ApplicationController
       @investments = []
       @table = CSV.parse(File.read("app/assets/docs/origen.csv"), headers: true)
       @table.each do |row|
-        investment = Investment.new(coin: row["Moneda"], interest: row["Interes_mensual"].to_f, balance_init: row["balance_ini"].to_f)
+        investment     = Investment.new(coin: row["Moneda"], interest: row["Interes_mensual"].to_f, balance_init: row["balance_ini"].to_f)
+        investment.pay = ((investment.interest / 100)*investment.balance_init)*12
         @investments.push(investment)
       end
     rescue => e
--- app/models/investment.rb
+++ app/models/investment.rb
@@ -4,7 +4,8 @@ class Investment
   field :coin, type: String
   field :interest, type: Float
   field :balance_init, type: Float
+  field :pay, type: Float
 
   validates :coin, :interest, :balance_init, presence: true
-  validates :interest, :balance_init, numericality: { greater_than: 0 }
+  validates :interest, :balance_init, :pay, numericality: { greater_than: 0 }
 end
